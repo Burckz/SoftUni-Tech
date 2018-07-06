017603cbfabb72a29fdd2b46028d224aaee07fc6
Burckz <40893269+Burckz@users.noreply.github.com>
2018-07-06 17:01:58 +0300

    Add files via upload

---
+++ advanced loop2/advanced loop2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace advanced_loop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            int i = 1;
+            if (a > b)
+            {
+                var result = Math..Floor(a / b);
+
+                if(i<=a)
+                {
+                    i++;
+                    
+                }
+                
+
+
+            }
+
+        }
+    }
+}
